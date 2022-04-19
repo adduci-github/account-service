6ae58182286e49f96fcc353f2716f5952d4deab7
정윤종 <>
2022-04-19 15:25:12 +0900

    몽고디비 플러그인 초기화 및 접속 정보 수정

--- src/main/java/com/example/accountservice/AccountServiceApplication.java
+++ src/main/java/com/example/accountservice/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
